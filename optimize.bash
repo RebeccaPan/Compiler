ecac5410fd3759784811644c001511fa821576dc
潘新怡 <rebeccapan@panxinyideMacBook-Air.local>
2021-04-12 12:18:27 +0800

    4.12 Add opt bash

--- optimize.bash
+++ optimize.bash
@@ -1,11 +1,3 @@
-# this script is called when the judge wants our compiler to compile a source file.
-# print the compiled source, i.e. asm code, directly to stdout.
-# don't print anything other to stdout.
-# if you would like to print some debug information, please go to stderr.
-
+set ff=UNIX
 set -e
-cd "$(dirname "$0")"
-# export CCHK="java -classpath ./lib/antlr-4.7.2-complete.jar:./bin Main"
-# cat > code.txt
-# $CCHK
-cat | java -Xss10m -ea -classpath ./lib/antlr-4.7.2-complete.jar:./bin Main codegen -O2
+cat | java -cp /ulib/java/antlr-4.9-complete.jar:./bin Main -codegen
