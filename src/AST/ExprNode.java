c0a6a46b5017c483457d2e2a3a1fa50f1d042082
潘新怡 <rebeccapan@panxinyideMacBook-Air.local>
2021-04-26 23:04:14 +0800

    4.26 Add constBroadcast

--- src/AST/ExprNode.java
+++ src/AST/ExprNode.java
@@ -8,10 +8,9 @@ abstract public class ExprNode extends ASTNode{
     private Type type;
     public enum ExprCat {LVal, RVal, Class, Func};
     private ExprCat exprCat;
+    protected ExprNode constVal;
 
-    public ExprNode(LocationType _location) {
-        super(_location);
-    }
+    public ExprNode(LocationType _location) { super(_location); constVal = null; }
     public Type getType() { return type; }
     public void setType(Type type) { this.type = type; }
     public ExprCat getExprCat() { return exprCat; }
@@ -20,4 +19,7 @@ abstract public class ExprNode extends ASTNode{
         if (!(exprCat == ExprCat.LVal || exprCat == ExprCat.RVal))
             throw new CompilationError("Expr not as LVal or RVal", curLoc);
     }
+    public boolean hasConstVal() { return constVal != null; }
+    public ExprNode getConstVal() { return constVal; }
+    public void setConstVal(ExprNode constVal) { this.constVal = constVal; }
 }
